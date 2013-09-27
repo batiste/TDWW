1ae0b281de1cc0778275a7853f16e15f8102cdc1
Darren Kaste <darrenkaste@gmail.com>
2013-09-27 13:49:38 -0400

    Simple test

---
+++ base.h
@@ -0,0 +1,8 @@
+#ifndef _BASE_H
+#define _BASE_H
+
+#include <iostream>
+
+typedef unsigned int uint;
+
+#endif //_BASE_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+#include "base.h"
 
 int main( int argc, char *argv[] ) {
+	for( uint i = 0; i < 100; i++ ) {
+		std::cout << "Testing... " << i << std::endl;
+	}
 	return 0;
 }
