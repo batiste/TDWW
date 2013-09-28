d37277f55febf4c2252c19bf474dfb829a339071
Darren Kaste <darrenkaste@gmail.com>
2013-09-27 21:11:56 -0400

    Starting development of ECS

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "base.h"
 #include "game.h"
+#include "menu_state.h"
 
 int main( int argc, char *argv[] ) {
 	Game &g = Game::getInstance();
 	g.init();
+	g.switchState( new MenuState );
 	g.run();
 	g.cleanup();
 	return 0;
